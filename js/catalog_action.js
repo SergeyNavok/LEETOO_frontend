a4320bd1e2fb730d5cefab8334eb95a287a9c71f
SergeyNavok <poketa@tut.by>
2022-01-15 13:10:00 +0300

    feat: dropdown action

--- js/catalog_action.js
+++ js/catalog_action.js
@@ -1,6 +1,6 @@
 const catalog = [
     {
-        'name': 'Сахарная паста для шугаринга LEETOO CLASSIC LIGHT (Мягкая)',
+        'name': 'Сахарная паста для шугаринга "LEETOO" CLASSIC ULTRALIGHT (Ультрамягкая)',
         'id': 1,
         'category': 'уход за кожей',
         'desription': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fuga consequuntur explicabo quos quibusdam animi rerum quidem',
@@ -8,7 +8,7 @@ const catalog = [
         'weight': 500
     },
     {
-        'name': 'Сахарная LEETOO CLASSIC LIGHT',
+        'name': 'Сахарная паста для шугаринга "LEETOO" CLASSIC LIGHT (Мягкая)',
         'id': 2,
         'category': 'уход за кожей',
         'desription': 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
@@ -16,11 +16,43 @@ const catalog = [
         'weight': 1500
     },
     {
-        'name': 'Сахарная паста для шугаринга LEETOO CLASSIC LIGHT (Мягкая)',
+        'name': 'Сахарная паста для шугаринга "LEETOO" CLASSIC MIDDLE (Средняя)',
         'id': 3,
         'category': 'гели',
         'desription': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fuga consequuntur explicabo quos quibusdam animi rerum quidem repudiandae nisi libero perspiciatis saepe repellendus voluptatem, natus, alias suscipit enim! Consequatur, eveniet!',
         'price': 100,
+        'weight': 700
+    },
+    {
+        'name': 'Сахарная паста для шугаринга "LEETOO" CLASSIC HARD (Плотная)',
+        'id': 4,
+        'category': 'гели',
+        'desription': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fuga consequuntur explicabo quos quibusdam animi rerum quidem repudiandae nisi libero perspiciatis saepe repellendus voluptatem, natus, alias suscipit enim! Consequatur, eveniet!',
+        'price': 70,
+        'weight': 500
+    },
+    {
+        'name': 'Сахарная паста Bandage Ананас (Бандажная)',
+        'id': 5,
+        'category': 'уход за кожей',
+        'desription': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fuga consequuntur explicabo quos quibusdam animi rerum quidem repudiandae nisi libero perspiciatis saepe repellendus voluptatem, natus, alias suscipit enim! Consequatur, eveniet!',
+        'price': 100,
+        'weight': 500
+    },
+    {
+        'name': 'Сахарная паста UltraLight Киви (Ультрамягкая)',
+        'id': 6,
+        'category': 'гели',
+        'desription': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fuga consequuntur explicabo quos quibusdam animi rerum quidem repudiandae nisi libero perspiciatis saepe repellendus voluptatem, natus, alias suscipit enim! Consequatur, eveniet!',
+        'price': 89,
+        'weight': 500
+    },
+    {
+        'name': 'Сахарная паста UltraLight Клубника (Ультрамягкая)',
+        'id': 7,
+        'category': 'уход за кожей',
+        'desription': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fuga consequuntur explicabo quos quibusdam animi rerum quidem repudiandae nisi libero perspiciatis saepe repellendus voluptatem, natus, alias suscipit enim! Consequatur, eveniet!',
+        'price': 99,
         'weight': 500
     }
 ]
@@ -29,9 +61,7 @@ const catalog = [
 
 //Куда будут записываться сформированные единицы продукции
 //Обращался по id, потому как по-другому не удавалось достучаться
-const productsTag = document.querySelector('#products');
-const currencyType = ' BYN';
-
+const productsTag = document.getElementById('products');
 
 //Функция создания элементов на странице
 function getNewTag(tagName, className, content = null) {
@@ -42,16 +72,17 @@ function getNewTag(tagName, className, content = null) {
     return newTag;
 }
 
-
-
 //Функция формирования разметки продукта
 function getProductItem(product) {
     const productBlock = getNewTag('div', 'products__block');
     const productBlockItem = getNewTag('div', 'products__block-item');
     const productImg = getNewTag('img', 'img');
+
     productImg.src = '..//img/product/' + product.id + '/product.jpg';
+    productImg.alt = product.name;
+
     const productName = getNewTag('div', 'name', product.name);
-    const productPrice = getNewTag('div', 'price', product.price + currencyType);
+    const productPrice = getNewTag('div', 'price', product.price + ' BYN');
 
     productBlockItem.append(productImg, productName, productPrice);
     productBlock.append(productBlockItem);
@@ -59,20 +90,81 @@ function getProductItem(product) {
     return productBlock;
 }
 
-
-
 //Функция получения продуктов из каталога
 function renderCatalog(catalog) {
-    const product = catalog.map(item => getProductItem(item));
+    productsTag.innerHTML = '';
+    const catalogCopy = catalog;
+    const product = catalogCopy.map(item => getProductItem(item));
     productsTag.append(...product);
+}
 
-    //catalog.forEach(item => {
-            //const product = getProductItem(item);
-            //productsTag.append(product);
-        //});
+//Запуск функции отрисовки каталога
+renderCatalog(catalog);
+
+
+
+
+
+//Обработка выпадающего меню
+const catalogDropdown = document.getElementById('dropdown_menu');
+
+//Обработчик события
+catalogDropdown.addEventListener('change', changeCategory);
+const dropdownCategoryDefault = 'по умолчанию';
+const dropdownCategoryAsc = 'сначала подешевле';
+const dropdownCategoryDesc = 'сначала подороже';
+
+
+
+//Функция обработки события
+function changeCategory(event) {
+    const category = event.target.value;
+    const catalogCopy = catalog;
+
+    switch (category) {
+        case dropdownCategoryDefault:
+            renderCatalog(catalogCopy);
+            break;
+
+        case dropdownCategoryAsc:
+            const catalogAsc = catalogCopy.sort((a, b) => a.price - b.price);
+            renderCatalog(catalogAsc);
+            break;
+
+        case dropdownCategoryDesc:
+            const catalogDesc = catalogCopy.sort((a, b) => b.price - a.price);
+            renderCatalog(catalogDesc);
+            break;
+
+        default:
+            const catalogFiltered = catalogCopy.filter(item => item.category === category);
+            renderCatalog(catalogFiltered);
+    }
 }
 
 
 
-//Запуск функции отрисовки каталога
-renderCatalog(catalog);
+//Функция добавления категрий в выпадающее меню
+function returnNewTagOption(nameCategory) {
+    const newTagOption = document.createElement('option');
+    newTagOption.value = nameCategory;
+    newTagOption.innerText = nameCategory;
+
+    return newTagOption;
+}
+
+function renderDropdown(catalog) {
+    catalogDropdown.innerHTML = '';
+    catalogDropdown.append(returnNewTagOption(dropdownCategoryDefault));
+    catalogDropdown.append(returnNewTagOption(dropdownCategoryAsc));
+    catalogDropdown.append(returnNewTagOption(dropdownCategoryDesc));
+
+    const catalogCopy = catalog;
+    
+    const categoryProduct = new Set(catalogCopy.map(item => item.category));
+    for (let item of categoryProduct) {
+        catalogDropdown.append(returnNewTagOption(item));
+    }
+}
+
+renderDropdown(catalog);
